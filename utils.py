f629c45957055f8d022227f7f950ff3a6f25e3f0
Carl Schader <carlschader@gmail.com>
2024-02-16 19:13:51 -0600

    fixed visualizer

--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
 import cv2, requests
 import numpy as np
 from io import BytesIO
+from PIL import Image
 
 def load_im(location):
   img = None
   # If location is a url
   if location.startswith('http'):
       response = requests.get(location)
-      img = location.open(BytesIO(response.content))
+      img = Image.open(BytesIO(response.content))
       img = np.array(img)
   else:
       img = cv2.imread(location)
